8cf6777f64ffc34f25f7f189ec0c866c2c907a23
nicktriada <nicktriada@gmail.com>
2022-09-05 01:03:36 -0700

    cleanup commit

---
+++ rev01_test_mirror/index.js
@@ -0,0 +1,20 @@
+console.log("TEST");  // print to console
+// window.alert("TEST"); // pop up allert
+
+// decalarion and assignhment of the variables 
+// var , let, const - declaration 
+// -  "="  - assibhnment 
+
+let age  = 21;
+console.log(age);
+age = age+1;
+
+let nameF = "My name is: ";
+let student = true;
+
+
+console.log(age);
+console.log(nameF);
+console.log(student);
+
+document.getElementById("p1").innerHTML = "Hello " + nameF
